1498670883644c11f0702b42d35a49cc6e5e17e0
Anshultilondiya <43814313+Anshultilondiya@users.noreply.github.com>
2020-08-03 14:01:11 +0530

    Removing Unnecessary Files

--- Sentiment Analysis/auth.py
+++ Sentiment Analysis/auth.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-
 consumer_key="cFyhr0WYTleOv1GF0WJiTNFRf"
 consumer_secret="gbA1X9qcBU2iMYduvU33n3groFipC4Vav60Wi6rEF1rkPvaQ3Y"
 access_token="1252935788448235520-mb14qtzDYXpxfbgQET14vOiYmw1SXd"
 access_token_secret="Ksz4SdIJTkoQjKChlVLN8426zgYdTGQzRJ8bJKvGyJtMR"
-
-=======
-consumer_key="cFyhr0WYTleOv1GF0WJiTNFRf"
-consumer_secret="gbA1X9qcBU2iMYduvU33n3groFipC4Vav60Wi6rEF1rkPvaQ3Y"
-access_token="1252935788448235520-mb14qtzDYXpxfbgQET14vOiYmw1SXd"
-access_token_secret="Ksz4SdIJTkoQjKChlVLN8426zgYdTGQzRJ8bJKvGyJtMR"
->>>>>>> b0f8d2c9cb69ff391d120ee6ab0842ecc51f06e7
--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tweepy
 import pandas as pd
 import sys
@@ -157,7 +156,3 @@ def sentiment_analysis(Companies_Names_Array):
             print("File Written Successfully")
 
     print_output(output)
-=======
-import tweets_catcher
-import textblob as TB 
->>>>>>> b0f8d2c9cb69ff391d120ee6ab0842ecc51f06e7
